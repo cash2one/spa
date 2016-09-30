2b5d61b6e9e0277aa50689b0910a7037a20a5e61
zhangjialin <jialin2013@qq.com>
2016-09-30 18:12:20 +0800

    6

---
+++ src/routers/robProjectDetail.vue
@@ -0,0 +1,18 @@
+<style>
+    @import '../styles/page/login.css';
+</style>
+<template>
+    <div class="loading" v-show="$loadingRouteData"><i></i><i></i><i></i></div>
+    <div class="page login-page" id="register-page" v-show="!$loadingRouteData">
+        <div class="page-title"><a class="back" @click="doClickPageBack()"></a>注册</div>
+
+    </div>
+</template>
+<script>
+    import { Global } from '../libs/global';
+    import Util from "../libs/util";
+
+    module.exports = {
+
+    }
+</script>
